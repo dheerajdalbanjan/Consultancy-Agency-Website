70d79e7dbc0f5f7a63f58e67cf67fc847d3a60cc
unknown <dheerajdalbanjan0403@gmail.com>
2023-12-09 16:53:49 +0530

    sidebar added

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -6,13 +6,50 @@ import { Button } from '@/components/ui/button'
 import { UseScrollHook } from '@/hooks/UseScrollTop'
 import { cn } from '@/lib/utils'
 import { ModeToggle } from '@/components/dark-mode'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from 'lucide-react'
+import Link from 'next/link'
+
 
 const Navbar = () => {
   const scrolled = UseScrollHook() ;
 
   return (
-    <div className={cn(scrolled &&"border-b dark:border-gray-600 shadow-sm", 'flex w-full z-50  items-center justify-between  backdrop-blur supports-[backdrop-filter]:bg-background/90 bg-background  px-6 md:px-12 py-4 fixed top-0')}>
+    <div className={cn(scrolled &&"border-b dark:border-gray-600 shadow-sm", 'flex w-full z-50  items-center justify-between  backdrop-blur supports-[backdrop-filter]:bg-background bg-background  px-6 md:px-12 py-4 fixed top-0')}>
+      <div  className='flex gap-x-3 items-center justify-center'>
+      <Sheet >
+  <SheetTrigger>
+  <Button variant={'outline'} className='bg-transparent px-2 py-1'>
+  <Menu />
+  </Button>
+  </SheetTrigger>
+  <SheetContent side={'left'} className='h-full w-full'>
+    <SheetHeader className='h-full'>
+      <SheetTitle>OurSouls</SheetTitle>
+      <SheetDescription className='flex h-full w-full flex-col gap-y-8 items-start justify-between'>
+        <div className='flex flex-col gap-y-4'>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/'}>About</Link>
+          <Link href={'/'}>Contact</Link>
+        </div>
+        <div className='flex items-center justify-between mt-auto gap-x-4 ml-auto'>
+          <Button variant='outline'>Login</Button>
+          <Button>SignUp</Button>
+        </div>
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+
         <Logo />
+        </div>
         <div className='flex gap-x-2 md:gap-x-4 items-center justify-center'>
             <Button >Login</Button>
             <ModeToggle />
