b7769465ec34233e3982b20bb6e30873be3697e9
unknown <dheerajdalbanjan0403@gmail.com>
2023-12-10 22:05:29 +0530

    minor fixes

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -9,37 +9,37 @@ type dataType ={
 const nodemailer = require('nodemailer')
 
 export async function POST(req: NextRequest){
-//     const data  = await req.json();
-//     console.log(data)
+    const data  = await req.json();
+    console.log(data)
 
-//     const {name, email, message}  = data; 
+    const {name, email, message}  = data; 
 
-//   const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     port: 465,
-//   secure: true,
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+  secure: true,
     
-//     auth: {
-//       user: 'oursouls04@gmail.com',
-//       pass: 'vtcm lbww uaik lxbb'
-//     }
-//   });
+    auth: {
+      user: 'oursouls04@gmail.com',
+      pass: 'vtcm lbww uaik lxbb'
+    }
+  });
 
-//   const mailOptions = {
-//     from: email,
-//     to: 'oursouls04@gmail.com',
-//     subject: `${name} tried to contact you with ${email}`,
-//     text: message
-//   };
+  const mailOptions = {
+    from: email,
+    to: 'oursouls04@gmail.com',
+    subject: `${name} tried to contact you with ${email}`,
+    text: message
+  };
 
-//   transporter.sendMail(mailOptions, (error: any, info : any) => {
-//     if (error) {
-//       console.log(error);
-//       return new Response('Erro', {status: 500})
-//     } else {
-//       console.log('Email sent: ' + info.response);
-//       return new Response('Success', {status: 200})
-//     }
-//   });
+  transporter.sendMail(mailOptions, (error: any, info : any) => {
+    if (error) {
+      console.log(error);
+      return new Response('Erro', {status: 500})
+    } else {
+      console.log('Email sent: ' + info.response);
+      return new Response('Success', {status: 200})
+    }
+  });
     return new Response('okay')
 }
