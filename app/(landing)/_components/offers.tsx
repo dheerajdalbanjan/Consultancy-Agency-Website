f6ee42fde5390ebaee059bf1b8db4b50c3ed238a
unknown <dheerajdalbanjan0403@gmail.com>
2024-01-29 23:48:14 +0530

    avail offer added

--- app/(landing)/_components/offers.tsx
+++ app/(landing)/_components/offers.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Image from "next/image";
 
@@ -27,15 +28,18 @@ const Offers = () => {
                     {offers.map((_, index) => (
                         <CarouselItem key={index}>
                             <div className="p-1 ">
-                                <Card className="overflow-hidden group">
+                                <Card className="overflow-hidden group relative">
                                     <CardHeader className="z-10">
                                         <CardTitle>{_.title}</CardTitle>
                                         <CardDescription>{_.description}</CardDescription>
                                     </CardHeader>
-                                    <CardContent className="flex aspect-square p-0 items-center justify-center relative   overflow-hidden">
+                                    <CardContent className="flex group-hover:opacity-70 group-hover:blur-sm transition-all aspect-square p-0 items-center justify-center relative   overflow-hidden">
                                         <div className="h-full absolute top-0  w-full bg-gradient-to-b from-neutral-50 dark:from-neutral-950 to-slate-50/10 z-10"></div>
                                         <Image src={_.image} alt="offer_image" width={0} height={0} sizes="100vw" className="w-full h-full object-fill group-hover:scale-110 transition-all duration-300 -z-0"></Image>
                                     </CardContent>
+                                    <CardFooter className="group-hover:scale-100 scale-0 transition-all absolute bottom-[25%] left-[19%]">
+                                        <a href="/contact"><button className="bg-transparent px-8 py-3 border-neutral-500 dark:border-neutral-200  active:scale-90 transition-all duration-200 text-neutral-700 dark:text-neutral-200 font-bold bg-opacity-80 border ">Avail Offer</button></a>
+                                    </CardFooter>
                                 </Card>
                             </div>
                         </CarouselItem>
