bf7e36598d47cdf494a7ce9a2173f494706874d3
unknown <dheerajdalbanjan0403@gmail.com>
2024-01-29 20:30:59 +0530

    terms and conditions added

--- app/(authentication)/signup/page.tsx
+++ app/(authentication)/signup/page.tsx
@@ -1,121 +1,179 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { CardContent, CardFooter } from '@/components/ui/card'
-import Formerror from '@/components/ui/formerror'
-import Formsuccess from '@/components/ui/formsuccess'
-import { Input } from '@/components/ui/input'
-import { cn } from '@/lib/utils'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { ReloadIcon } from '@radix-ui/react-icons'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+"use client";
+import { Button } from "@/components/ui/button";
+import { CardContent, CardFooter } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import Formerror from "@/components/ui/formerror";
+import Formsuccess from "@/components/ui/formsuccess";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-
-const formSchema = z.object({
-  name: z.string().min(5).max(50),
-  email: z.string().email(),
-  password: z.string().length(8, {message:"The password must be of length 8"}) ,
-  confirmPass: z.string().length(8, {message:"The password must be of length 8"})
-}).refine(data => data.confirmPass === data.password , {message:"password and confirm password do not match", path: ["confirmPass"]})
-
-
+const formSchema = z
+  .object({
+    name: z.string().min(5).max(50),
+    email: z.string().email(),
+    password: z
+      .string()
+      .length(8, { message: "The password must be of length 8" }),
+    confirmPass: z
+      .string()
+      .length(8, { message: "The password must be of length 8" }),
+  })
+  .refine((data) => data.confirmPass === data.password, {
+    message: "password and confirm password do not match",
+    path: ["confirmPass"],
+  });
 
 const Signup = () => {
-  const router = useRouter() ;
-  const {register, handleSubmit, formState:{errors} , reset} = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema)
-  }); 
-  const [error, setError] = useState(""); 
-  const [loading , setLoading] = useState(false) ;
-  const [success, setSuccess] = useState(false) ; 
-  const submitt =async (value:z.infer<typeof formSchema>) => {
-    setLoading(true)
-    const res = await fetch('/api/userExist', {
-      method: 'POST', 
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const submitt = async (value: z.infer<typeof formSchema>) => {
+    setLoading(true);
+    const res = await fetch("/api/userExist", {
+      method: "POST",
       headers: {
-        'content-type': 'application/json'
-      }, 
-      body: JSON.stringify({email: value.email})
-    })
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ email: value.email }),
+    });
 
-    const {user} = await res.json() ;
-    console.log(user + " :userexist")
+    const { user } = await res.json();
+    console.log(user + " :userexist");
 
-    if(user){
-      setError("The Email Id already exists.")
-      setLoading(false)
-      return ;
+    if (user) {
+      setError("The Email Id already exists.");
+      setLoading(false);
+      return;
     }
 
-    const response = await fetch('/api/signup', {
-      method: 'POST',
-      headers:{
-        'content-type': 'application/json'
-      }, 
-      body: JSON.stringify(value)
-    }); 
-    setSuccess(true)
-    setLoading(false)
-    if(response.ok){
-      console.log("signup success")
-      router.push('/login')
+    const response = await fetch("/api/signup", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(value),
+    });
+    setSuccess(true);
+    setLoading(false);
+    if (response.ok) {
+      console.log("signup success");
+      router.push("/login");
+    } else {
+      console.log("error occured while signing up");
     }
-    else {
-      console.log("error occured while signing up")
-    }
-  }
+  };
   return (
     <div>
-      <form action="" method='POST' onSubmit={handleSubmit(submitt)} >
+      <form action="" method="POST" onSubmit={handleSubmit(submitt)}>
         <CardContent>
-      <div className='flex flex-col gap-y-2  my-4'>
-          <label className={cn(errors.name && 'text-red-500')}>Full Name</label>
-          <Input  {...register('name')} type='text' placeholder='eg: virat kohli' />
-          {errors.name && <span className="text-[0.9rem] text-red-500 antialised">{errors.name.message}</span>}
-        </div>
-      <div className='flex flex-col gap-y-2  my-4'>
-          <label className={cn(errors.email && 'text-red-500')}>Email</label>
-          <Input  {...register('email')} type='text' placeholder='eg: viratkohli@gmail.com' />
-          {errors.email && <span className="text-[0.9rem] text-red-500 antialised">{errors.email.message}</span>}
-        </div>
-      <div className='flex flex-col gap-y-2  my-4'>
-          <label className={cn(errors.password && 'text-red-500')}>Password</label>
-          <Input  {...register('password')} type='password' placeholder='eg: virat kohli' />
-          {errors.password && <span className="text-[0.9rem] text-red-500 antialised">{errors.password.message}</span>}
-        </div>
-      <div className='flex flex-col gap-y-2  my-4'>
-          <label className={cn(errors.confirmPass && 'text-red-500')}>Confirm Password</label>
-          <Input  {...register('confirmPass')} type='password' placeholder='eg: virat kohli' />
-          {errors.confirmPass && <span className="text-[0.9rem] text-red-500 antialised">{errors.confirmPass.message}</span>}
-        </div>
-        <Link href='login' className='text-right  text-sm py-5 '>
-          Existing User  <span className='underline'>Login</span>
-        </Link>
-        {
-          error && <Formerror error={error}></Formerror>
-        }
-        {success && <div className='py-2'>
-        <Formsuccess msg="Successfully Signed Up"></Formsuccess>
-      </div>}
-      </CardContent>
+          <div className="flex flex-col gap-y-2  my-4">
+            <label className={cn(errors.name && "text-red-500")}>
+              Full Name
+            </label>
+            <Input
+              {...register("name")}
+              type="text"
+              placeholder="eg: virat kohli"
+            />
+            {errors.name && (
+              <span className="text-[0.9rem] text-red-500 antialised">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-y-2  my-4">
+            <label className={cn(errors.email && "text-red-500")}>Email</label>
+            <Input
+              {...register("email")}
+              type="text"
+              placeholder="eg: viratkohli@gmail.com"
+            />
+            {errors.email && (
+              <span className="text-[0.9rem] text-red-500 antialised">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-y-2  my-4">
+            <label className={cn(errors.password && "text-red-500")}>
+              Password
+            </label>
+            <Input
+              {...register("password")}
+              type="password"
+              placeholder="eg: virat kohli"
+            />
+            {errors.password && (
+              <span className="text-[0.9rem] text-red-500 antialised">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-y-2  my-4">
+            <label className={cn(errors.confirmPass && "text-red-500")}>
+              Confirm Password
+            </label>
+            <Input
+              {...register("confirmPass")}
+              type="password"
+              placeholder="eg: virat kohli"
+            />
+            {errors.confirmPass && (
+              <span className="text-[0.9rem] text-red-500 antialised">
+                {errors.confirmPass.message}
+              </span>
+            )}
+          </div>
+          <div className="flex items-center space-x-2 my-5">
+            <Checkbox id="terms" required/>
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Accept <a href="/termsandconditions" className="underline">terms and conditions</a>
+            </label>
+          </div>
+          <Link href="login" className="text-right  text-sm py-5 ">
+            Existing User <span className="underline">Login</span>
+          </Link>
 
-      <CardFooter>
-      <Button type='submit' className='w-full' disabled={loading?true:false} >
+          {error && <Formerror error={error}></Formerror>}
+          {success && (
+            <div className="py-2">
+              <Formsuccess msg="Successfully Signed Up"></Formsuccess>
+            </div>
+          )}
+        </CardContent>
+
+        <CardFooter>
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={loading ? true : false}
+          >
             {loading && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
-            Sign up</Button>
-      </CardFooter>
-        
-
-        
-
-        
-      </form>  
-      
+            Sign up
+          </Button>
+        </CardFooter>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
---
+++ app/termsandconditions/page.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+        <div className="max-w-3xl mx-auto  dark:bg-neutral-950 p-6  rounded-md shadow">
+        <h1 className="text-3xl font-semibold mb-6">Refund Policy</h1>
+
+        <p className="mb-4">Last Updated: January 29, 2024</p>
+
+        <p className="mb-4">We aim to provide caring and professional counseling services to all clients. However, we
+            understand there may be rare instances where a refund is requested. Here is our policy on refunds:</p>
+
+        <h2 className="text-2xl font-semibold mb-4">Individual Sessions</h2>
+        <ul className="list-disc pl-6 mb-4">
+            <li>Refund requests must be made within 7 days of session date.</li>
+            <li>Refunds are provided if sessions could not be delivered due to technical issues on our end or
+                counselor unavailability.</li>
+            <li>Refunds are not provided if the client misses or needs to reschedule the session.</li>
+        </ul>
+
+        <h2 className="text-2xl font-semibold mb-4">Session Packages</h2>
+        <ul className="list-disc pl-6 mb-4">
+            <li>Unused sessions in pre-paid packages are non-refundable but remain valid for future use during the
+                active subscription term.</li>
+            <li>Should a subscription need to be canceled early, prorated refunds may be issued for any full unused
+                months remaining minus an early termination fee equal to 1 month’s subscription rate.</li>
+            <li>Refunds are not offered for custom or discounted subscription packages unless contractually specified
+                otherwise.</li>
+        </ul>
+
+        <h2 className="text-2xl font-semibold mb-4">Payment Refunds</h2>
+        <ul className="list-disc pl-6 mb-4">
+            <li>Refunds are generally issued back to the original payment method used. Processing time may take 7-14
+                days.</li>
+            <li>Refund amounts will exclude any payment provider or platform fees which are non-refundable.</li>
+        </ul>
+
+        <h2 className="text-2xl font-semibold mb-4">General</h2>
+        <ul className="list-disc pl-6 mb-4">
+            <li>All refund requests must be submitted via email to <a href="mailto:support@oursoulss.com"
+                    className="text-blue-500">oursoulss04@gmail.com</a> for review.</li>
+            <li>Refund eligibility is determined at the sole discretion of site management based on policies here.</li>
+            <li>By using our services, you understand all sales final unless a refund is provided under terms
+                herein.</li>
+        </ul>
+
+        <p className="mb-4">We will try to be understanding of impactful extenuating circumstances and make limited
+            exceptions if reasonable and feasible. However, our general position is that sales of consultation services
+            are final and not eligible for refunds or chargebacks after delivery. We appreciate your understanding.
+            Please reach out if you have any questions!</p>
+    </div>
+    </div>
+  )
+}
+
+export default page
