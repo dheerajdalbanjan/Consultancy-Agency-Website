bba59381f08800ee2ad902d3946e38cb776790ec
unknown <dheerajdalbanjan0403@gmail.com>
2024-02-29 23:29:50 +0530

    offer update

--- app/(landing)/_components/offers.tsx
+++ app/(landing)/_components/offers.tsx
@@ -7,14 +7,9 @@ const Offers = () => {
 
     const offers = [
         {
-            title: "First 30 Minutes Free", 
-            description:"This is a 1 Month of Oursoulss Offer and is Valid only for 26th Feb.", 
-            image: "https://i.ibb.co/t3GSSCD/photo-6257929524398374520-y.jpg"
-        },
-        {
-            title: "Word Peace Day, first 11 minutes free", 
-            description: 'This offer is valid from 23rd Feb to 29th Feb.', 
-            image: "https://i.ibb.co/gJQFYcx/Picsart-24-02-23-14-46-51-932.jpg"
+            title: "World Happiness Day Offer", 
+            description: 'It is celebrated on March 20, first 11 minutes free till 20th March', 
+            image: "https://i.ibb.co/m8hR7mm/Designer-3.png"
         }
         
     ] ;
